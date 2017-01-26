3a14fe4133a83d24b41b24e5ffbd68e439bb8311
Ravi Prakash Mishra <ravi.nitp.ece@gmail.com>
2017-01-25 19:47:30 -0500

    use extra byte array from intent to get byte array directly

--- app/src/main/java/com/example/rpmishra/lang/ClarifaiUtil.java
+++ app/src/main/java/com/example/rpmishra/lang/ClarifaiUtil.java
@@ -11,6 +11,7 @@ package com.example.rpmishra.lang;
         import android.content.Intent;
         import android.graphics.Bitmap;
         import android.graphics.BitmapFactory;
+        import android.os.Bundle;
         import android.support.annotation.NonNull;
         import android.support.annotation.Nullable;
         import android.view.View;
@@ -37,6 +38,10 @@ public final class ClarifaiUtil {
     public static byte[] retrieveSelectedImage(@NonNull Context context, @NonNull Intent data) {
         InputStream inStream = null;
         Bitmap bitmap = null;
+
+        //byte[] byteArray = data.getByteArrayExtra("capture");
+        bitmap  = (Bitmap) data.getExtras().get("data");
+
         try {
             inStream = context.getContentResolver().openInputStream(data.getData());
             bitmap = BitmapFactory.decodeStream(inStream);
